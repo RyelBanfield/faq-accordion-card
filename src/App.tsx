9b634a5c551517474bc6afc4702b84421d4dded1
Ryel Banfield <ryelbanfield@gmail.com>
2022-08-21 13:24:44 -0400

    Add Mobile Illustration and Shadow

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-// # Front-end Style Guide
-
-// ## Layout
-
-// The designs were created to the following widths:
-
-// - Mobile: 375px
-// - Desktop: 1440px
-
-// ## Colors
-
-// ### Primary
-
-// #### Text
-
-// - Very dark desaturated blue: hsl(238, 29%, 16%)
-// - Soft red: hsl(14, 88%, 65%)
-
-// ### Neutral
-
-// #### Text
-
-// - Very dark grayish blue: hsl(237, 12%, 33%)
-// - Dark grayish blue: hsl(240, 6%, 50%)
-
-// #### Dividers
-
-// - Light grayish blue: hsl(240, 5%, 91%)
+/* eslint-disable import/no-absolute-path */
+import Shadow from '/src/assets/bg-pattern-mobile.svg';
+import MobileIllustration from '/src/assets/illustration-woman-online-mobile.svg';
 
 const App = () => (
   <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-b from-soft-violet to-soft-blue text-xs">
-    <div className="h-3/4 w-3/4 rounded-3xl bg-white" />
+    <div className="h-[540px] w-[330px] rounded-3xl bg-white">
+      <div className="relative flex justify-center">
+        <img
+          src={MobileIllustration}
+          alt="Illustration of woman online"
+          className="absolute -top-[105px] w-[230px]"
+        />
+        <img src={Shadow} alt="Shadow" />
+      </div>
+    </div>
   </div>
 );
 
